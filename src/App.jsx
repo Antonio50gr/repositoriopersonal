bbc7c3404788412552babbc4dc76ae41f405b32c
Antonio50gr <angaro1926@gmail.com>
2024-08-14 15:59:56 +0200

    5 commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="relative min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       {/* Fondo con gradiente radial */}
-      <div className="fixed top-0 left-0 z-[-1] w-full h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="fixed top-0 left-0 z-[-1] w-full min-h-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
       {/* Contenido principal */}
       <div className="relative z-10">
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
       <div className="flex flex-col lg:flex-row">
         {/* Contenedor del texto */}
         <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-start bg-transparent">
-          <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+          <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl bg-transparent">
             Antonio García Rodríguez
           </h1>
-          <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+          <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent bg-transparent">
             Full Stack Developer
           </span>
           <p className="my-2 max-w-xl py-6 font-light tracking-tighter bg-transparent">
