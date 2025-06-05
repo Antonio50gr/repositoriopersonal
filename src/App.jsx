d781725a2eaebed7004633e1e37ce8cb952cca18
Antonio50gr <angaro1926@gmail.com>
2025-06-05 16:02:19 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+
 import Navbar from './component/Navbar';
 import Hero from './component/Hero';
 import About from './component/About';
@@ -6,7 +8,43 @@ import Technologies from './component/Technologies';
 import Projects from './component/Projects';
 import Contact from './component/Contact';
 
-const App = () => {
+const GA_TRACKING_ID = 'G-458DFP7KKQ';
+
+function loadGA() {
+  if (!window.gtag) {
+    const script1 = document.createElement('script');
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+    document.head.appendChild(script1);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    window.gtag = gtag;
+
+    gtag('js', new Date());
+    gtag('config', GA_TRACKING_ID);
+  }
+}
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    loadGA();
+  }, []);
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('config', GA_TRACKING_ID, {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
+
+const AppContent = () => {
   return (
     <div className="relative min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       {/* Fondo con gradiente radial */}
@@ -14,17 +52,24 @@ const App = () => {
 
       {/* Contenido principal */}
       <div className="relative z-10">
-        <div className='container mx-auto px-8'>
+        <div className="container mx-auto px-8">
           <Navbar />
           <Hero />
           <About />
           <Technologies />
           <Projects />
-          <Contact/>
+          <Contact />
         </div>
       </div>
     </div>
   );
 };
+
+const App = () => (
+  <Router>
+    <AnalyticsTracker />
+    <AppContent />
+  </Router>
+);
 
 export default App;
